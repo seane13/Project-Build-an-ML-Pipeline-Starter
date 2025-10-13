caf1ad2554b53e6eaebaaefb098c854048f98783
seane13 <seanendicott76@gmail.com>
2025-10-12 19:10:34 -0700

    fix step reference

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def go(config: DictConfig):
                 json.dump(dict(config["modeling"]["random_forest"]), fp)
 
             _ = mlflow.run(
-                "./src/train_random_forest",
+                "https://github.com/seane13/Project-Build-an-ML-Pipeline-Starter#components/train_random_forest",
                 "main",
                 parameters={
                     "trainval_artifact": "trainval_data.csv:latest",
